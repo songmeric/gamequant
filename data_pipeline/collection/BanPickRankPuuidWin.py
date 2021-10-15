13ff1c1ff1ffb412701ebf9bf7e547d463e9bd8a
sumuli02 <76789665+sumuli02@users.noreply.github.com>
2021-10-15 14:00:23 +0900

    Update and rename ChampionLane_new.py to BanPickRankPuuidWin.py

--- data_pipeline/collection/BanPickRankPuuidWin.py
+++ data_pipeline/collection/BanPickRankPuuidWin.py
@@ -1,14 +1,23 @@
-
 from riotwatcher import LolWatcher, RiotWatcher
+"""
+Return data structure:
+Stored in "match_data_ban.csv"
+[gameID], [Ban], [BluePick], [RedPick], [ParticipantTier], [ParticipantPuuid], [AverageTier], [Win(Blue)]
+Put '[' and ']' at both ends and pass it throuhg ast.literal_eval(<data>) to get read the result as a list
+"""
 
-game = []
+
 """
-#reading gameId list
-with open("game_ban1.csv", "r") as f:
-    game = f.read().split(", ")
-f.close()
+Make sure the "gameId_reply.csv" file is in the same folder. or change the code below.
 """
+with open("gameId_reply.csv", "r") as f:
+    Game = f.read().split(",")
+Game = Game[:-1]
+
 #Initial Setting for API (change this later)
+"""
+Make sure all the api keys below are activated
+"""
 config = {"api_id": "RGAPI-8d5e0759-7754-4f27-8178-531c4ec07883",
           "api_id_1": "RGAPI-7f88bc6b-a4dc-47af-bcbb-166d08bc7494",
           "api_id_2": "RGAPI-91df06f3-cf41-4621-9fb5-27807266e81e",
@@ -25,13 +34,8 @@ config = {"api_id": "RGAPI-8d5e0759-7754-4f27-8178-531c4ec07883",
 
 region = "ASIA"
 
-with open("gameId_reply.csv", "r") as f:
-    Game = f.read().split(",")
-Game = Game[:-1]
-
 for i in range(len(Game)):
-    x = i % 13
-    x= 0
+    x = i % len(list(config.keys()))
     api = LolWatcher(config[list(config.keys())[x]])
 
     gameId = Game[i]
@@ -48,7 +52,6 @@ for i in range(len(Game)):
             add = api.league.by_summoner("kr", add1)[0]["tier"]
         except:
             summonerName = RiotWatcher(config[list(config.keys())[x]]).account.by_puuid("ASIA", participants_puuid[i])["gameName"]
-            #print(summonerName)
             add1 = api.summoner.by_name("kr",summonerName)["id"]
             add = api.league.by_summoner("kr", add1)[0]["tier"]
 
@@ -96,15 +99,16 @@ for i in range(len(Game)):
                         Num_tier += 1
                 else:
                     Num_tier += j
-    Tier = ["C","GM", "M", "D", "P", "GO", "S", "B"]
+    Tier = ["C","GM", "M", "D", "P", "G", "S", "B"]
     Tier = Tier[int(Num_tier/Total_tier)]
 
     if Blue[0] == Blue[1]:
         continue
 
-    with open("match_data_ban1.csv", "a") as f:
+    with open("match_data_ban.csv", "a") as f:
         line = str([gameId]) + ","  + str(Ban) + "," + str(Blue) + "," + str(Red) + "," + str(participants_rank) + "," + str(participants_puuid)+","+ str([Tier]) + ","+ str([Win]) + "\n"
         f.write(line)
-
+    
+    #Comment out this if unneccessary
     print("check")
 
